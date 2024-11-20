b7ab9f990a2a9e6c83113f2b46bfebbf71d0c2e4
Dartegnian <root@dartegnian.com>
2024-11-21 01:50:02 +0800

    Add EC2 instance and configure basic security group

--- 1/main.tf
+++ 1/main.tf
@@ -56,3 +56,44 @@ resource "aws_route_table_association" "public" {
   subnet_id = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+# Security Group
+resource "aws_security_group" "ec2" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "cupa-ec2-security-group"
+  }
+}
+
+# EC2 instance
+resource "aws_instance" "web" {
+  ami = "ami-0c02fb55956c7d316" # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public.id
+  security_groups = [
+    aws_security_group.ec2.name
+  ]
+  tags = {
+    Name = "web-cupa-instance"
+  }
+}
